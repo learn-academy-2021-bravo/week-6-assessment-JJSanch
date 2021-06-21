91a6e534b78c08595ae9bf9ccb945efec88b08fc
JJSanch <jon.sanchez003@gmail.com>
2021-06-21 08:56:14 -0700

    initial commit

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,10 @@
 // ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
 
+// const { expect } = require("@jest/globals")
+// const { it } = require("jest-circus")
+// const { resolveTestEnvironment } = require("jest-resolve")
+// const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -16,6 +21,7 @@
 
 // --------------------1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their names capitalized.
 
+
 // a) Create a test with an expect statement using the variable provided.
 
 var people = [
@@ -23,6 +29,20 @@ var people = [
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "radio employee" }
 ]
+//created a new variable as an array to pass into the function
+var peopleComboArray = ["ford prefect" , "hitchhiker" ,
+"zaphod beeblebrox" , "president of the galaxy" , "arthur dent" , "radio employee"]
+
+//describing the function name here
+describe("when capNameArray is called", () => {
+//describing what the function does
+  test("it returns an array with a sentence about each person with their names capitalized", () => {
+    //describing the expected outcome
+    expect(capNameArray()).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is a president of the galaxy.", "Arthus Dent is a radio employee."])  
+  })
+})
+// expect("capitalizeName")   
+
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is a president of the galaxy.", "Arthus Dent is a radio employee."]
 
 
@@ -30,9 +50,12 @@ var people = [
 
 
 // b) Create the function that makes the test pass.
+//attempted to use higher order function to map across the array and join the sentences together as string pushed into one array to get expected output
 
+const capNameArray = (array) =>  array.map(arrayItem => typeof arrayItem === "string").push.join("")
 
-
+console.log(capNameArray(peopleComboArray));
+//I was not able to get my test to pass with this function
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
@@ -44,14 +67,27 @@ var hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 var hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+//describing the function na,e
+describe("remaindersOnly", () => {
 
+//used multiple let statements to declare the inputs and expected outputs
+  let hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+  let hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+  let expectedOutput1 = [ 2, 0, -1, 0 ]
+  let expectedOutput2 = [ 2, 1, -1 ]
 
-
+  //used an it statement to describe exactly what this text is doing, aslong with expect statements and dot notation to describe expected inputs and outputs
+    it("returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
+      expect(remaindersOnly(hodgepodge1)).toEqual(expectedOutput1)
+      expect(remaindersOnly(hodgepodge2)).toEqual(expectedOutput2)
+    })
+})
+    
 
 // b) Create the function that makes the test pass.
 
-
-
+const remaindersOnly = (array) =>  array.map(arrayItem => arrayItem % 3)
+//created a function that maps across arrays and returns the remainder of numbers divided by 3.  Was not able to get the test to pass
 
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
@@ -61,10 +97,28 @@ var cubeAndSum1 = [2, 3, 4]
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+
+
+
 // a) Create a test with an expect statement using the variables provided.
 
+//describing the function name here
+describe("numbersCubed", () => {
+  //created let statements to describe the inputs and outpots
+  let cubeAndSum1 = [2, 3, 4]
+  let cubeAndSum2 = [0, 5, 10]
+  let expectedNumber1 = 99
+  let expectedNumber2 = 1125
 
+  //used it to describe what the function is doing
+    it("returns the sum of all the numbers cubed", () => {
+      expect(numbersCubed(cubeAndSum1)).toEqual(expectedNumber1)
+      expect(numbersCubed(cubeAndSum2)).toEqual(expectedNumber2)
+    })
+})
 
 
 
 // b) Create the function that makes the test pass.
+//created a function that takes in the input arrays and returns the sum of all numbers cubed.  However was not able to get test to pass.
+const numbersCubed = (array) =>  array.map(arrayItem => typeof arrayItem * 3).sum
--- interview-questions.md
+++ interview-questions.md
@@ -7,44 +7,90 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating an API with a model called Animal that has_many Sightings, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be part of the Animal model or the Sightings model?
 
-  Your answer:
+  Your answer: The foreign key would be part of the animal model.  The foreign key would link the two models
 
-  Researched answer:
+  Researched answer: The foreign key links the animal and the sightings model.  The foreign key belongs to the sightings modelm and links it to the animal model. To create a the foreign key, one would use the command FOREIGN KEY (column name) REFERENCES Table Name(column name).
 
 
 
 2. Which RESTful routes must always be passed params? Why?
 
-  Your answer:
+  Your answer: Delete and Destroy
 
-  Researched answer:
+  Researched answer: delete, /photos/:id, /:id, photos#, destory.
+  These routes must be passed params to manage what goes in and out of the database and to keep unwanted information out of the database.
 
 
 
 3. Name three rails generator commands. What is created by each?
 
-  Your answer:
+  Your answer: 
+               gem install rails - this installs the rails directory structure
 
-  Researched answer:
+               rails server - this allows you to access your app through a browser
+
+               db:migrate - this migrates changes you made to the a rails databse
+
+  Researched answer: 
+                     rails db:create - sets up an empty databse
+
+                     rails new app_name - generates new folder with enture Rails 
+                     directory sructure.  All code needed to run app is included in this command
+
+                     rails generate rseource - sets up a migration and a model needed to use something as an ActiveRecord class
 
 
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
-method="GET"    /students          
+  a. method="GET"    /students   
 
-method="GET"    /students/2  
+    Controller method is index.  This will display a list of all students     
 
-method="GET"    /students/new
+  b.method="GET"    /students/2  
 
-method="GET"    /students/edit/2    
+    Controller method is show. Displays the second student on the list
 
-method="POST"   /students/       
+  c. method="GET"    /students/new
 
-method="PATCH"  /students/2      
+    Controller method is new.  Returns an HTML form for creating a new student.
 
-method="DELETE" /students/2      
+  d. method="GET"    /students/edit/2    
+
+    Controller method is edit.  Returns a form for editing the second student on the list.
+
+  e. method="POST"   /students/       
+
+    Controller method is create.  This would create a new student.
+
+  f. method="PATCH"  /students/2      
+
+    Controller method is update.  This would update information on the second student on the students list.
+
+  g. method="DELETE" /students/2    
+
+    Controller method is destroy.  This would delete a specific a student from the list.
 
 
 
 5. As a developer, you want to make an application that will help you manage your to do list. To get started, you want to create the API. Create 10 user stories that will help you get your application started.
+
+  1. As a user, I can see a landing page when navigating to the app 
+
+  2.  As a user, I can navigate from the landing page to the home page
+
+  3.  As a user, I can create or delete to-do lists on the home page
+
+  4.  As a user, I can click on "create to-do lists" and be routed to a page where I can create a new to do list
+
+  5. As a user, I can click on already created to do lists to either delete or edit them
+
+  6. As a user, I can see a form to create a new to do list.
+
+  7. As a user, I can naivgate out of the form and back to the home page.
+
+  8. As a user, I can click on a button that will save my created to do list.
+
+  9. As a user, I can click on a button that will edit an existing to do list.
+
+  10. As a user, I can click on the title of the app at the top of the screen and be routed to the app home page.
